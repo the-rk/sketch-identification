dbeb77b67c55ac4719e9ecb5f13dad09bcd84995
the-rk <95013057+the-rk@users.noreply.github.com>
2022-06-02 15:38:37 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -2,7 +2,34 @@ function setup(){
     canvas = createCanvas(250,250);
     canvas.center();
     background("white");
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesis;
 }
 function clear(){
     background("white");
+}
+function preload(){
+classifier = ml5.imageClassifier('DoodleNet');
+}
+function draw(){
+    strokeWeight(13);
+    stroke("red");
+    if (mouseIsPressed){
+    line(pmouseX,pmouseY,mouseX,mouseY);
+    }
+}
+function classifyCanvas(){
+    classifier.classify(canvas,gotresult);
+}
+function gotresult(error,results){
+if (error){
+    console.error(error);
+}
+else{
+  console.log(results);
+  document.getElementById("label").innerHTML = "label: "+ results[0].label;
+  document.getElementById("confidence").innerHTML = "confidence: "+ Math.round(results[0].confidence *100)+"%";
+  utterThis = new speachSynthesisUtterance(results[0].label);
+  synth.speak(utterThis);
+}
 }
